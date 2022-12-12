5db4b176b5c9628d4002b09f23ce2136359deece
weide <4a339047@stust.edu.tw>
2022-12-12 14:17:01 +0800

    增加刪除功能和增加增加了兩個函式is_image(),dummy_icon()

--- api/upload.php
+++ api/upload.php
@@ -1,15 +1,15 @@
 <?php
 include_once "base.php";
 if($_FILES['file_name']['error']==0){
-    $file_str_array=explode(".",$_FILES['file_name']['name']);
-$sub=array_pop($file_str_array);
+    $file_str_array=explode(".",$_FILES['file_name']['name']);//explode將字串分割成多個元素
+$sub=array_pop($file_str_array);//將檔案的附檔名抓出來(因為array_pop可以彈出陣列裡最後的元素)
 $file_name=date("Ymdhis").".".$sub;//改檔名
 // move_uploaded_file($_FILES['file_name']['tmp_name'],"../upload/".$_FILES['file_name']['name']);
-move_uploaded_file($_FILES['file_name']['tmp_name'],"../upload/".$file_name);
+move_uploaded_file($_FILES['file_name']['tmp_name'],"../upload/".$file_name);//移動檔案到upload資料夾裡
 // $description=$_POST['description'];
 // echo $description;
 // echo"<br>";
-insert('upload',['description'=>$_POST['description'], //增加到資料表裡
+insert('upload',['description'=>$_POST['description'], //上傳的檔案增加到資料表裡
                  'size'=>$_FILES['file_name']['size'],
                  'type'=>$_FILES['file_name']['type'],
                  'file_name'=>$file_name]);
--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
  * 3.搬移檔案
  * 4.顯示檔案列表
  */
-
+include_once "./api/base.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +15,50 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+         .list{
+            list-style-type:none;
+            padding: 0;
+            margin:1rem auto;
+            padding-top:1px;
+            width:80%;
+
+        }
+        .list-item{
+            display: flex;
+            align-items: center;
+            border: 1px solid #aaa;
+            margin-top:-1px ;
+        }
+        .list-item:nth-child(1){
+            text-align: center;
+            background-color: lightgreen;
+        }
+        .list-item div:nth-child(1){
+            width:20%;
+        }
+        .list-item div:nth-child(1) img{
+            width:150px;
+            height:100px;
+        }
+        .list-item div:nth-child(2){
+            width:20%;
+        }
+        .list-item div:nth-child(3){
+            width:20%;
+        }
+        .list-item div:nth-child(4){
+            width:10%;
+        }
+        .list-item div:nth-child(5){
+            width:20%;
+            overflow: hidden;
+        }
+        .list-item div:nth-child(6){
+            width:10%;
+            overflow: hidden;
+        }
+    </style>
 </head>
 <body>
  <h1 class="header">檔案上傳練習</h1>
@@ -37,7 +81,56 @@
 
 
 <!----建立一個連結來查看上傳後的圖檔---->  
+<?php
+$files=all('upload');
+if(count($files)>0){
+echo "<ul class='list'>";
+echo "<li class='list-item'>";
+echo "<div>縮圖</div>";
+echo "<div>描述</div>";
+echo "<div>檔名</div>";
+echo "<div>大小</div>";
+echo "<div>類型</div>";
+echo "<div>操作</div>";
+echo "</li>";
+    foreach($files as $file){
+        echo "<li class='list-item'>";
+            echo "<div>";
+            if(is_image($file['type'])){
+                echo "<img src='./upload/{$file['file_name']}'>";
+                
+            }else{
+                $icon=dummy_icon($file['type']);
+                echo "<img src='./material/{$icon}' style='width:80px'>";
 
+            }
+           
+            echo "</div>";
+            echo "<div>";
+            echo $file['description'];
+            echo "</div>";
+            echo "<div>";
+            echo $file['file_name'];
+            echo "</div>";
+            echo "<div>";
+            echo floor($file['size']/1024)."mb";
+            echo "</div>";
+            echo "<div>";
+            echo $file['type'];
+            echo "</div>";
+            echo "<div>";
+            echo "<a href='edit_form.php?id={$file['id']}'>編輯</a>";
+            echo "<a href='./api/del.php?id={$file['id']}'>刪除</a>";
+            echo "</div>";
+
+        echo "</li>";
+    }
+echo "</ul>";
+}else{
+    echo "目前尚無上傳資料";
+}
+
+?>
 
 </body>
 </html>
